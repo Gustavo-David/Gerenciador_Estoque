97b842c34032df92c193d24367f54e34fa5d26dc
Gustavo-David <gustavodavidpinto@gmail.com>
2024-11-23 18:47:36 -0300

    Create Class Products and Entities

---
+++ src/main/java/com/GerenciadoEstoque/Entities/Products.java
@@ -0,0 +1,106 @@
+package com.GerenciadoEstoque.Entities;
+
+import com.GerenciadoEstoque.EntitiesStatus.ProductsCategory;
+
+public class Products {
+    private Long id;
+    private String name;
+    private String description;
+    private Integer quantity;
+    private Double buyPrice;
+    private Double shellPrive;
+    private ProductsCategory category;
+
+    public Products() {
+
+    }
+
+    public Products(String name, String description, Integer quantity, Double buyPrice, Double shellPrive, ProductsCategory category) {
+        this.name = name;
+        this.description = description;
+        this.quantity = quantity;
+        this.buyPrice = buyPrice;
+        this.shellPrive = shellPrive;
+        this.category = category;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    public Double getBuyPrice() {
+        return buyPrice;
+    }
+
+    public void setBuyPrice(Double buyPrice) {
+        this.buyPrice = buyPrice;
+    }
+
+    public Double getShellPrive() {
+        return shellPrive;
+    }
+
+    public void setShellPrive(Double shellPrive) {
+        this.shellPrive = shellPrive;
+    }
+
+    public ProductsCategory getCategory() {
+        return category;
+    }
+
+    public void setCategory(ProductsCategory category) {
+        this.category = category;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Products other = (Products) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Products [id=" + id + ", name=" + name + ", description=" + description + ", quantity=" + quantity + ", buyPrice=" + buyPrice + ", shellPrive=" + shellPrive + ", category=" + category + "]";
+    }
+    
+
+}
---
+++ src/main/java/com/GerenciadoEstoque/EntitiesStatus/ProductsCategory.java
@@ -0,0 +1,10 @@
+package com.GerenciadoEstoque.EntitiesStatus;
+
+public enum ProductsCategory {
+    SMARTPHONES_AND_ACCESSORIES,
+    COMPUTERS_AND_LAPTOPS,
+    TELEVISIONS,
+    AUDIO_DEVICES,
+    APPLIANCES,
+    POWER_TOOLS;
+}
